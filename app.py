17ce1127b36e25b9b37a46fe3ce9af0b0d1a0cee
Arman Khan <armankhan52210@gmail.com>
2025-06-17 13:55:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -127,6 +127,10 @@ def index_page():
                 #  return jsonify({"error":response})
             
             
+# Server par Flask port dynamically handle karna
+import os
 
-if __name__=="__main__":
-    app.run(debug=False,port=5001)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
+
