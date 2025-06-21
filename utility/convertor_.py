0b89f1bcf7d044b1d69796dc1da13817cafdd2b3
Arman Khan <armankhan52210@gmail.com>
2025-06-22 02:37:44 +0530

    Update convertor_.py

--- utility/convertor_.py
+++ utility/convertor_.py
@@ -1,7 +1,7 @@
-print("convertor executing...")
+# print("convertor executing...")
 
 import json # convert data to json
-from utility import NewSyntax as NS
+from utility.NewSyntax import *
 
 # convert to oops
 class finish_response:
@@ -15,23 +15,31 @@ class finish_response:
       This will give a behaviour to object
       """
       try:
-        print("try in convertor")
-        prompt1 = self.prompt
-        returnedresponse = NS.search_output(prompt1) # NS_obj.return_rspo_opt() will return string llm response
+        # print("try in convertor")
+        # print(f"IN convertor file prompt : {self.prompt}")
+        # prompt1 = self.prompt
+        # returnedresponse = NS.search_output(prompt1) # NS_obj.return_rspo_opt() will return string llm response
+        NewSyntax_object = search_output(self.prompt) # NS_obj.return_rspo_opt() will return string llm response
+        returnedresponse = NewSyntax_object.search_output() 
 
-        print("before converting",type(returnedresponse)) # check the response datatype returned by llm model
+        # print("before converting",type(returnedresponse)) # check the response datatype returned by llm model
         data = json.loads(returnedresponse) # loads to the json formate to change the datatype
         if type(data) == str(): # if still the type if string then again pass the prompt to the finish_response class
-          print(f"Type is string - {type(data)}\nAgain generating response!")
+          # print(f"Type is string - {type(data)}\nAgain generating response!")
           prompt2 = self.prompt
-          response = NS.search_output(prompt2)
+          NSobject = search_output(self.prompt)
+          response = NSobject.search_output()
 
-        print("After parsing into json - ",type(data)) # if data loaded to json successfully
+        # print("After parsing into json - ",type(data)) # if data loaded to json successfully
       
-      except: # if try block raise an error when loading to the json
-          print("Passed in except")
-          print("An Error occured while parsing into json(try)")
+      except Exception as E: # if try block raise an error when loading to the json
+          # print("Passed in except")
+          # print("An Error occured while parsing into json(try)")
+          # response=search_output(prompt) 
+          # print("before parsing into json - ",type(response))
+          # data = json.loads(response)
           x = "Sorry, can you please enter your query again😉!"
+          # x = str(E)
           data = {"response":x}
 
       return data
@@ -39,4 +47,4 @@ class finish_response:
 
 ########################
 
-print("convertor end executing...")
+# print("convertor end executing...")
